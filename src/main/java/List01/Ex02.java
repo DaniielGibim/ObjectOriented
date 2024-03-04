ebf2f18b60dfa81454f50bc5675f6273ab65d835
Daniel Gibim <DanielGibimdev@gmail.com>
2024-03-04 12:39:29 -0800

    Resolução 1 e 2

---
+++ src/main/java/List01/Ex02.java
@@ -0,0 +1,43 @@
+package List01;
+
+import java.util.Scanner;
+
+/*
+    Sejam a, b e c os três lados de um triângulo. Elabore um algoritmo que verifica se o triângulo é:
+
+    Escaleno (todos os lados diferentes)
+    Isósceles (possui dois lados iguais)
+    Equilátero (todos os lados iguais)
+    Não forma triângulo (a soma de dois lados deve ser maior que o terceiro lado)
+    Os três números devem ser inteiros positivos e lidos na mesma linha (Ex: 3 4 5) Exemplos de entrada e saída esperada:
+
+    Exemplo 1: Entrada = 2 3 2 | Saída = Isosceles
+    Exemplo 2: Entrada = 2 2 2 | Saída = Equilatero
+    Exemplo 3: Entrada = 1 2 4 | Saída = Nao forma triangulo
+    Exemplo 4: Entrada = -1 2 4 | Saída = Erro
+    Qualquer valor fora do domínio de entrada tem como saída esperada a String "Erro".
+*/
+public class Ex02 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Ex02 ex02 = new Ex02();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(ex02.compute(a,b,c));
+        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        //Escreva o resultado da chamada do método compute() aqui
+    }
+
+    String compute(int a, int b, int c) {
+
+        if (a < 0 || b < 0 || c < 0) return "Erro";
+        if (a + b < c) return "Não forma Triângulo";
+
+        if (a == b && a == c) return "Equilatero";
+        if (a == b || b == c || a == c) return "Isosceles";
+        return "Escaleno";
+
+    }
+}
